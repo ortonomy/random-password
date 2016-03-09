1d07309eee71dd5b0d02fc96773df51b245aaf44
xenographer <github@xenographer.com>
2016-03-09 12:16:19 +0800

    First commit!

---
+++ RandomPassword/MainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.swift
+//  RandomPassword
+//
+//  Created by Gregory Orton on 09/03/2016.
+//  Copyright © 2016 Parabola. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
